df1ac4773326bf5cf93a2d9d691da192bb26041d
yone521 <82038608+yone521@users.noreply.github.com>
2021-04-08 01:17:15 +0900

    Add files via upload

---
+++ likeActivity.java
@@ -0,0 +1,14 @@
+package kr.sswu.croquischallenge;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class likeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceSate) {
+
+        super.onCreate(savedInstanceSate);
+        setContentView(R.layout.like_main);
+    }
+}
